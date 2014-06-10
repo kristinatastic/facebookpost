fffb62c8418af89dd293b658eaccf9a48e8ab178
Kristina Varshavskaya <kristinatastic@kristinatas-mbp.thefacebook.com>
2014-06-10 13:42:45 -0700

    Initial Commit

---
+++ facebookpost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  facebookpost
+//
+//  Created by Kristina Varshavskaya on 6/10/14.
+//  Copyright (c) 2014 kristinatastic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
